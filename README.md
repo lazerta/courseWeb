9c7092ec7f279bf04e1dbb728564b7e46925e960
= <linxiang08@gmail.com>
2018-09-12 16:23:40 -0400

    1

---
+++ README.md
@@ -0,0 +1,1 @@
+this is for my 365 coursev
--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-let navbar =document.querySelector('.nav-bar');
-let target = document.querySelector(".fa-bars");
-let hidden = document.querySelector(".nav-hidden");
-let vis = navbar.style.visibility;
-target.addEventListener('clcik',function(){
- if(vis === 'hidden' ) vis = 'visible';
- vis = 'hidden'; 
-});
-alert('msg');
 
+// kmap 
