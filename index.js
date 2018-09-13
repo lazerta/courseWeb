9fa7331bfdd83e74f977113d60ec27a50ddf5888
= <linxiang08@gmail.com>
2018-09-13 15:49:10 -0400

    assignment 0 added

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let bar = document.querySelector(".fa-bars");
+let nav = document.querySelector(".nav-bar");
+let info = document.getElementById("getInfo");
+let display = document.querySelector('.display');
 
-// kmap 
+info.addEventListener('click',() => {
+    if(display.innerHTML === ""){
+        display.innerHTML = navigator.appName;
+    }else {
+        display.innerHTML = "";
+    }
+});
+
+
+bar.addEventListener("click", function() {
+ nav.classList.toggle('ds');
+
+});
+
